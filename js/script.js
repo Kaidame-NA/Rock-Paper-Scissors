db67cf72d496374f36779a7fdeb9161f0f7f4bb0
Edward Wang <ewang43@jhu.edu>
2022-01-04 21:10:51 -0500

    Finish playRound function

--- js/script.js
+++ js/script.js
@@ -4,15 +4,37 @@ function computerPlay()
 	switch(rng)
 	{
 		case 0:
-			return "Rock";
+			return "ROCK";
 			break;
 		case 1:
-			return "Paper";
+			return "PAPER";
 			break;
 		case 2:
-			return "Scissors";
+			return "SCISSORS";
 			break;
 	}
 }
 
-
+function playRound(playerSelection, computerSelection)
+{
+	playerSelection = playerSelection.toUpperCase();
+	if(playerSelection === computerSelection)
+	{
+		return "Tie! You both threw " + playerSelection.toLowerCase();
+	}
+	
+	switch(playerSelection)
+	{
+		case "ROCK":
+			return computerSelection === "PAPER" ? "You Lose! Paper beats Rock" : "You Win! Rock beats Scissors"
+			break;
+		case "PAPER":
+			return computerSelection === "SCISSORS" ? "You Lose! Scissors beats Paper" : "You Win! Paper beats Rock"
+			break;
+		case "SCISSORS":
+			return computerSelection === "ROCK" ? "You Lose! Rock beats Scissors" : "You Win! Scissors beats Paper" 
+			break;
+		default:
+			return "Invalid Choice";
+	}
+}
